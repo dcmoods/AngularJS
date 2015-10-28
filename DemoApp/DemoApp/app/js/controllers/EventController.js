74ce6da6eeb785f36556c8cc7ce7deadc9aefe70
Michael Moody <dcmoods@gmail.com>
2015-10-27 21:22:18 -0400

    Added DemoApp

---
+++ DemoApp/DemoApp/app/js/controllers/EventController.js
@@ -0,0 +1,21 @@
+/**
+ * Created by moody on 10/27/2015.
+ */
+'use strict';
+
+eventsApp.controller('EventController',
+    function EventController($scope){
+        $scope.event = {
+            name: 'Angular Boot Camp',
+            date: '1/1/2016',
+            time: '12:30 pm',
+            location:{
+                address: '123 Main Street',
+                city: 'Mountain View',
+                province: 'CA'
+            }
+        }
+
+
+    }
+);
